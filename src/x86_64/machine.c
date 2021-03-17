8b4e53d56a7f0c052f4d395727ccad85f0f260ce
msi <support@kerbogames.com>
2021-03-17 20:09:56 +0100

    Bisqwit Fix

--- src/x86_64/machine.c
+++ src/x86_64/machine.c
@@ -1,14 +1,9 @@
 #include "machine.h"
 
 unsigned char ter_pack_rex(const bool w, const bool r, const bool x, const bool b) {
-	register unsigned char rex = 0x40;
-	rex ^= (-(unsigned char)b ^ rex) & 1 << 0;
-	rex ^= (-(unsigned char)x ^ rex) & 1 << 1;
-	rex ^= (-(unsigned char)r ^ rex) & 1 << 2;
-	rex ^= (-(unsigned char)w ^ rex) & 1 << 3;
-	return rex;
+	return (unsigned char)(0x40 | b | (unsigned char)x << 1 | (unsigned char)r << 2 | (unsigned char)w << 3);
 }
 
 unsigned char ter_pack_trio(const unsigned char bi01, const unsigned char b234, const unsigned char b567) {
-	return (unsigned char)(b567 & ~0xF8 | (b234 & ~0xF8) << 3 | (bi01 & ~0xFC) << 6);
+	return (unsigned char)((b567 & ~0xF8) | (b234 & ~0xF8) << 3 | (bi01 & ~0xFC) << 6);
 }
